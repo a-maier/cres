7399d152a179efbc189b5486d186e8cb58ef7d9a
Andreas Maier <andreas.martin.maier@desy.de>
2021-02-19 14:46:51 +0100

    added support for lz4 and zstd compression

--- src/bin/auto_decompress.rs
+++ src/bin/auto_decompress.rs
@@ -6,15 +6,23 @@ use log::debug;
 
 const GZIP_MAGIC_BYTES: [u8; 2] = [0x1f, 0x8b];
 const BZIP2_MAGIC_BYTES: [u8; 3] = [b'B', b'Z', b'h'];
+const ZSTD_MAGIC_BYTES: [u8; 4] = [0x37, 0xa4, 0x30, 0xec];
+const LZ4_MAGIC_BYTES: [u8; 4] = [0x04, 0x22, 0x4d, 0x18];
 
 pub fn auto_decompress<'a, B: 'a + BufRead>(mut r: B) -> Box<dyn BufRead + 'a> {
     let bytes = match r.fill_buf() {
         Ok(bytes) => bytes,
         Err(_) => return Box::new(r),
     };
-    if bytes.len() <= 3 {
+    if bytes.len() <= 4 {
         debug!("No decompression");
         Box::new(r)
+    } else if bytes[..4] == LZ4_MAGIC_BYTES {
+        debug!("Decompress as lz4");
+        Box::new(BufReader::new(lz4::Decoder::new(r).unwrap()))
+    } else if bytes[..4] == ZSTD_MAGIC_BYTES {
+        debug!("Decompress as zstd");
+        Box::new(BufReader::new(zstd::stream::Decoder::new(r).unwrap()))
     } else if bytes[..2] == GZIP_MAGIC_BYTES {
         debug!("Decompress as gzip");
         Box::new(BufReader::new(GzDecoder::new(r)))
