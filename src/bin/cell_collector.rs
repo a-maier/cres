5e801b7899455b14e8ee8e7fe0e67473e7eccd8d
Andreas Maier <andreas.martin.maier@desy.de>
2021-03-04 16:31:07 +0100

    bug fix: collect cells with same number of members/radius/weight

--- src/bin/cell_collector.rs
+++ src/bin/cell_collector.rs
@@ -15,9 +15,11 @@ const NCELLS: usize = 10;
 pub(crate) struct CellCollector {
     first: Vec<(usize, Vec<usize>)>,
     random: Vec<(usize, Vec<usize>)>,
-    largest_by_radius: BTreeMap<N64, (usize, Vec<usize>)>,
-    largest_by_members:  BTreeMap<usize, (usize, Vec<usize>)>,
-    largest_by_weight:  BTreeMap<N64, (usize, Vec<usize>)>,
+    // we keep the cell number as part of the key so that
+    // different cells with the same number of members can be kept
+    largest_by_radius: BTreeMap<(N64, usize), Vec<usize>>,
+    largest_by_members:  BTreeMap<(usize, usize), Vec<usize>>,
+    largest_by_weight:  BTreeMap<(N64, usize), Vec<usize>>,
     count: usize,
 }
 
@@ -46,27 +48,27 @@ impl CellCollector {
             let events = cell.iter().map(|(_d, e)| e.id);
             self.first.push((count, events.clone().collect()));
             self.random.push((count, events.clone().collect()));
-            self.largest_by_radius.insert(r, (count, events.clone().collect()));
-            self.largest_by_members.insert(nmembers, (count, events.clone().collect()));
-            self.largest_by_weight.insert(weight, (count, events.collect()));
+            self.largest_by_radius.insert((r, count), events.clone().collect());
+            self.largest_by_members.insert((nmembers, count), events.clone().collect());
+            self.largest_by_weight.insert((weight, count), events.collect());
         } else {
-            let smallest_r = *self.largest_by_radius.keys().next().unwrap();
+            let (smallest_r, n) = *self.largest_by_radius.keys().next().unwrap();
             if r > smallest_r {
-                self.largest_by_radius.remove(&smallest_r).unwrap();
+                self.largest_by_radius.remove(&(smallest_r, n)).unwrap();
                 let events = cell.iter().map(|(_d, e)| e.id).collect();
-                self.largest_by_radius.insert(r, (count, events));
+                self.largest_by_radius.insert((r, count), events);
             }
-            let least_members = *self.largest_by_members.keys().next().unwrap();
+            let (least_members, n) = *self.largest_by_members.keys().next().unwrap();
             if nmembers > least_members {
-                self.largest_by_members.remove(&least_members).unwrap();
+                self.largest_by_members.remove(&(least_members, n)).unwrap();
                 let events = cell.iter().map(|(_d, e)| e.id).collect();
-                self.largest_by_members.insert(nmembers, (count, events));
+                self.largest_by_members.insert((nmembers, count), events);
             }
-            let smallest_weight = *self.largest_by_weight.keys().next().unwrap();
+            let (smallest_weight, n) = *self.largest_by_weight.keys().next().unwrap();
             if weight > smallest_weight {
-                self.largest_by_weight.remove(&smallest_weight).unwrap();
+                self.largest_by_weight.remove(&(smallest_weight, n)).unwrap();
                 let events = cell.iter().map(|(_d, e)| e.id).collect();
-                self.largest_by_weight.insert(weight, (count, events));
+                self.largest_by_weight.insert((weight, count), events);
             }
             let distr = Uniform::from(0..=count);
             let idx = distr.sample(&mut rng);
@@ -84,15 +86,15 @@ impl CellCollector {
             info!("Cell {} with {} events", id, events.len());
         }
         info!("Largest cells by radius:");
-        for (r, (id, events)) in self.largest_by_radius.iter().rev() {
+        for ((r, id), events) in self.largest_by_radius.iter().rev() {
             info!("Cell {} with {} events and radius {}", id, events.len(), r);
         }
         info!("Largest cells by number of events:");
-        for (_, (id, events)) in self.largest_by_members.iter().rev() {
+        for ((_, id), events) in self.largest_by_members.iter().rev() {
             info!("Cell {} with {} events", id, events.len());
         }
         info!("Cells with largest accumulated weights:");
-        for (weight, (id, events)) in self.largest_by_weight.iter().rev() {
+        for ((weight, id), events) in self.largest_by_weight.iter().rev() {
             info!("Cell {} with {} events and weight {:e}", id, events.len(), weight);
         }
         info!("Randomly selected cells:");
@@ -103,14 +105,14 @@ impl CellCollector {
 
     pub fn event_cells(&self) -> HashMap<usize, Vec<usize>> {
         let mut result: HashMap<usize, Vec<_>> = HashMap::new();
-        let all_cells = self.first.iter()
-            .chain(self.random.iter())
-            .chain(self.largest_by_radius.iter().map(|(_r, id)| id))
-            .chain(self.largest_by_members.iter().map(|(_n, id)| id))
-            .chain(self.largest_by_weight.iter().map(|(_n, id)| id));
+        let all_cells = self.first.iter().map(|(id, events)| (*id, events))
+            .chain(self.random.iter().map(|(id, events)| (*id, events)))
+            .chain(self.largest_by_radius.iter().map(|((_r, id), events)| (*id, events)))
+            .chain(self.largest_by_members.iter().map(|((_n, id), events)| (*id, events)))
+            .chain(self.largest_by_weight.iter().map(|((_w, id), events)| (*id, events)));
         for (cell, event_ids) in  all_cells {
             for event_id in event_ids {
-                result.entry(*event_id).or_default().push(*cell)
+                result.entry(*event_id).or_default().push(cell)
             }
         }
         result
