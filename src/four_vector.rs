0fd279d6b710ec990c0ede9da60a3eb74503e160
Andreas Maier <andreas.martin.maier@desy.de>
2021-05-25 13:42:41 +0200

    method to get four vector transverse component

--- src/four_vector.rs
+++ src/four_vector.rs
@@ -1,7 +1,10 @@
 use noisy_float::prelude::*;
 
 #[derive(PartialEq, Eq, PartialOrd, Ord, Debug, Clone, Copy, Default)]
-pub struct FourVector([N64; 4]);
+pub struct FourVector{
+    p: [N64; 4],
+    pt: N64
+}
 
 impl FourVector {
     pub fn new() -> Self {
@@ -13,7 +16,7 @@ impl FourVector {
     }
 
     pub fn euclid_norm_sq(&self) -> N64 {
-        self.0.iter().map(|e| *e * *e).sum()
+        self.p.iter().map(|e| *e * *e).sum()
     }
 
     pub fn spatial_norm(&self) -> N64 {
@@ -21,17 +24,27 @@ impl FourVector {
     }
 
     pub fn spatial_norm_sq(&self) -> N64 {
-        self.0.iter().skip(1).map(|e| *e * *e).sum()
+        self.p.iter().skip(1).map(|e| *e * *e).sum()
+    }
+
+    pub fn pt(&self) -> N64 {
+        self.pt
     }
 
     const fn len() -> usize {
         4
     }
+
+    fn update_pt(&mut self) {
+        self.pt = (self.p[1] * self.p[1] + self.p[2] * self.p[2]).sqrt();
+    }
 }
 
 impl std::convert::From<[N64; 4]> for FourVector {
-    fn from(comp: [N64; 4]) -> FourVector {
-        FourVector(comp)
+    fn from(p: [N64; 4]) -> FourVector {
+        let mut res = FourVector{p, pt: std::default::Default::default()};
+        res.update_pt();
+        res
     }
 }
 
@@ -39,29 +52,25 @@ impl std::ops::Index<usize> for FourVector {
     type Output = N64;
 
     fn index(&self, i: usize) -> &Self::Output {
-        &self.0[i]
-    }
-}
-
-impl std::ops::IndexMut<usize> for FourVector {
-    fn index_mut(&mut self, i: usize) -> &mut N64 {
-        &mut self.0[i]
+        &self.p[i]
     }
 }
 
 impl std::ops::AddAssign for FourVector {
     fn add_assign(&mut self, rhs: FourVector) {
         for i in 0..Self::len() {
-            self[i] += rhs[i]
+            self.p[i] += rhs[i]
         }
+        self.update_pt();
     }
 }
 
 impl std::ops::SubAssign for FourVector {
     fn sub_assign(&mut self, rhs: FourVector) {
         for i in 0..Self::len() {
-            self[i] -= rhs[i]
+            self.p[i] -= rhs[i]
         }
+        self.update_pt();
     }
 }
 
