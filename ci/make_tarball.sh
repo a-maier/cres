c02b1b15bc933a73c33dfcd4bb42a12444cbf284
Andreas Maier <andreas.martin.maier@desy.de>
2023-03-27 17:19:44 +0200

    add shell completion to release package

--- ci/make_tarball.sh
+++ ci/make_tarball.sh
@@ -4,6 +4,7 @@ set -ex
 
 mkdir $PROJECT $PROJECT/bin $PROJECT/lib $PROJECT/include
 cp target/$1/release/$PROJECT $PROJECT/bin/
+cp target/$1/release/cres-generate-shell-completions $PROJECT/bin/
 cp target/$1/release/lib$PROJECT.* $PROJECT/lib/
 cp target/$1/release/build/*/out/cres.h $PROJECT/include/
 tar czf $PROJECT.tar.gz $PROJECT
