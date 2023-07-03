28353cc3d12957213bc746ac66c2d4e6deaedd5d
Andreas Maier <andreas.martin.maier@desy.de>
2023-07-03 09:55:50 +0200

    update dependencies

--- build.rs
+++ build.rs
@@ -4,15 +4,12 @@ use std::path::PathBuf;
 
 use anyhow::Result;
 use cbindgen::Language;
-use vergen::{vergen, Config, ShaKind};
+use vergen::EmitBuilder;
 
 fn main() -> Result<()> {
-    let mut cfg = Config::default();
-    *cfg.git_mut().sha_kind_mut() = ShaKind::Short;
-    // If this is not run inside a git repository we get an error.
-    // This happens when installing the crate via cargo.
-    // As a quick fix, we just ignore it.
-    let _ = vergen(cfg);
+    EmitBuilder::builder()
+        .git_sha(true)
+        .emit()?;
 
     if cfg!(target_family = "unix") {
         write_c_header()
