461a593e24e8d3d8ff6a046baa3ef01e9dff9353
Andreas Maier <andreas.martin.maier@desy.de>
2023-08-21 17:52:16 +0200

    work around Cargo bug

--- build.rs
+++ build.rs
@@ -22,7 +22,14 @@ fn main() -> Result<()> {
     }
 
     #[cfg(feature = "ntuple")]
-    for flag in ntuple::ROOT_LINKER_FLAGS {
+    // work around https://github.com/rust-lang/cargo/issues/12326
+    // once this is fixed, we can remove the code below and use
+    // ```
+    // for flag in ntuple::ROOT_LINKER_FLAGS {
+    //     println!("cargo:rustc-link-arg={flag}");
+    // }
+    // ```
+    for flag in get_root_flags("--libs")? {
         println!("cargo:rustc-link-arg={flag}");
     }
 
@@ -64,4 +71,30 @@ fn write_c_header() {
         .generate()
         .expect("Unable to generate bindings")
         .write_to_file(out);
+}
+
+fn get_root_flags(flags: &str) -> Result<Vec<String>> {
+    use anyhow::{bail, Context};
+    use std::{process::Command, str::from_utf8};
+
+    const CFG_CMD: &str = "root-config";
+
+    let cmd = format!("{CFG_CMD} {flags}");
+    let output = Command::new(CFG_CMD)
+        .arg(flags)
+        .output()
+        .with_context(|| format!("Failed to run `{cmd}`"))?;
+    if !output.status.success() {
+        if output.stderr.is_empty() {
+            bail!("{CFG_CMD} {flags} failed without error messages");
+        } else {
+            bail!(
+                "{CFG_CMD} {flags} failed: {}",
+                from_utf8(&output.stderr).unwrap()
+            );
+        }
+    }
+    let args = from_utf8(&output.stdout)
+        .with_context(|| format!("Failed to convert `{cmd}` output to utf8"))?;
+    Ok(args.split_whitespace().map(|arg| arg.to_owned()).collect())
 }
