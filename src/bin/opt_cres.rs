ac7f32b1dbc2a65581e80eb86ee3e9af280ca244
Andreas Maier <andreas.martin.maier@desy.de>
2024-07-24 10:07:53 +0200

    implement reconstruction of intermediate W bosons

--- src/bin/opt_cres.rs
+++ src/bin/opt_cres.rs
@@ -58,6 +58,7 @@ pub(crate) struct UnweightOpt {
 
 #[derive(Debug, Parser)]
 #[clap(about, author, version)]
+#[allow(non_snake_case)]
 pub(crate) struct Opt {
     /// Output directory.
     ///
@@ -135,6 +136,10 @@ variable."
     /// Whether to use an absolute or relative distance
     #[clap(long)]
     pub(crate) distance: DistanceType,
+
+    /// Reconstruct intermediate W bosons
+    #[clap(long)]
+    pub(crate) reconstruct_W: bool,
 
     /// Comma-separated list of weights to include in the resampling
     ///
