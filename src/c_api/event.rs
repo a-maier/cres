de2b3fd2886436b6b24992e7a3dfe2efb012fba9
Andreas Maier <amaier@ifae.es>
2025-01-31 08:51:32 +0100

    add conversion from `EventView` to `Event`

--- src/c_api/event.rs
+++ src/c_api/event.rs
@@ -1,3 +1,8 @@
+use crate::{
+    ParticleID,
+    event::{Event, EventBuilder}
+};
+
 use std::marker::PhantomData;
 use std::os::raw::c_double;
 
@@ -62,3 +67,48 @@ impl TypeSet {
 
 /// Four-momentum [E, px, py, pz]
 pub type FourMomentum = [c_double; 4];
+
+/// Convert from [EventView] to [Event]
+///
+/// # Safety
+///
+/// The input has to be valid, i.e. `weights` should point to a slice
+/// with at least `n_weights` elements and `type_sets` to a slice with
+/// at least `n_type_set` elements.
+///
+impl<'a> From<EventView<'a>> for Event {
+    fn from(view: EventView<'a>) -> Self {
+        use crate::n64;
+        let EventView {
+            id,
+            weights,
+            type_sets,
+            n_weights,
+            n_type_sets,
+        } = view;
+        let n_particles =
+            (0..n_type_sets)
+            .map(|n| unsafe { (*type_sets.add(n)).n_momenta })
+            .sum();
+        let mut event = EventBuilder::with_capacity(n_particles);
+        for n_weight in 0..n_weights {
+            let weight = unsafe { *weights.add(n_weight) };
+            event.add_weight(n64(weight));
+        }
+        for n_set in 0..n_type_sets {
+            let TypeSetView {
+                pid,
+                momenta,
+                n_momenta,
+                ..
+            } = unsafe { *type_sets.add(n_set) };
+            for n_p in 0..n_momenta {
+                let p = unsafe { *momenta.add(n_p) };
+                event.add_outgoing(ParticleID::new(pid), p.map(n64).into());
+            }
+        }
+        let mut event = event.build();
+        event.id = id;
+        event
+    }
+}
