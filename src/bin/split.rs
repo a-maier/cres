ad063271142f11187c2101791a214cc8bf71fc95
Andreas Maier <amaier@ifae.es>
2025-03-31 11:08:53 +0200

    add program to split event files by multiplicities

---
+++ src/bin/split.rs
@@ -0,0 +1,176 @@
+mod opt_common;
+mod opt_particle_def;
+mod opt_split;
+
+use std::{
+    collections::{hash_map::Entry, HashMap},
+    fs,
+    path::{Path, PathBuf},
+};
+
+use crate::opt_split::Opt;
+
+use anyhow::{Context, Result};
+use clap::Parser;
+use cres::{
+    event::Event, io::Converter, ntuple::NTupleConverter,
+    prelude::DefaultClustering, traits::Clustering, ParticleID, FEATURES,
+    GIT_BRANCH, GIT_REV, VERSION,
+};
+use env_logger::Env;
+use itertools::Itertools;
+use log::{debug, info};
+use opt_particle_def::ParticleDefinitions;
+
+fn main() -> Result<()> {
+    // TODO: code duplication with other cres binaries
+    let args = argfile::expand_args_from(
+        std::env::args_os(),
+        argfile::parse_fromfile,
+        argfile::PREFIX,
+    )
+    .with_context(|| "Failed to read argument file")?;
+    let opt = Opt::parse_from(args);
+    // TODO: validate!
+
+    let env = Env::default().filter_or("CRES_LOG", &opt.loglevel);
+    env_logger::init_from_env(env);
+
+    rayon::ThreadPoolBuilder::new()
+        .num_threads(opt.threads)
+        .build_global()?;
+
+    if let (Some(rev), Some(branch)) = (GIT_REV, GIT_BRANCH) {
+        info!(
+            "cres-split-by-multiplicity {VERSION} rev {rev} ({branch}) {FEATURES:?}"
+        );
+    } else {
+        info!("cres-split-by-multiplicity {VERSION} {FEATURES:?}");
+    }
+
+    debug!("settings: {:#?}", opt);
+
+    let converter = Converter::new();
+
+    let ParticleDefinitions {
+        jet_def,
+        lepton_def,
+        photon_def,
+        include_neutrinos,
+        min_missing_pt,
+        reconstruct_W,
+    } = opt.particle_def;
+    let mut clustering = DefaultClustering::new(jet_def.into())
+        .reconstruct_W(reconstruct_W)
+        .include_neutrinos(include_neutrinos)
+        .min_missing_pt(min_missing_pt);
+    if lepton_def.leptonalgorithm.is_some() {
+        clustering = clustering.with_lepton_def(lepton_def.into())
+    }
+    if photon_def.photonradius.is_some() {
+        clustering = clustering.with_photon_def(photon_def.into())
+    }
+
+    fs::create_dir_all(&opt.outdir)?;
+    for file in opt.infiles {
+        info!("Splitting up {file:?}");
+        let [_, file_stem, suffix] = split_filename(&file);
+
+        let reader = ntuple::Reader::new(&file).with_context(|| {
+            format!("Failed to read {file:?} as NTuple file")
+        })?;
+        let mut writers = HashMap::new();
+
+        for event in reader {
+            let event = event.with_context(|| {
+                format!("Failed to read event from {file:?}")
+            })?;
+            let internal = converter.convert_ntuple(event.clone())?;
+            let clustered = clustering.cluster(internal)?;
+            let multiplicities = multiplicities(&clustered);
+            match writers.entry(multiplicities) {
+                Entry::Vacant(v) => {
+                    let mult = v.key();
+                    let outfile_name =
+                        gen_outfile_name(file_stem, mult, suffix);
+                    let out_path = &opt.outdir.join(&outfile_name);
+                    let writer = ntuple::Writer::new(&out_path, "")
+                        .with_context(|| {
+                            format!(
+                                "Failed to write {out_path:?} as NTuple file"
+                            )
+                        })?;
+                    v.insert(writer).write(&event)?
+                }
+                Entry::Occupied(mut o) => o.get_mut().write(&event)?,
+            };
+        }
+    }
+    info!("Done");
+    Ok(())
+}
+
+#[cfg(unix)]
+fn gen_outfile_name(
+    file_stem: &Path,
+    mult: &[(ParticleID, usize)],
+    suffix: &Path,
+) -> PathBuf {
+    use std::ffi::OsStr;
+    use std::os::unix::ffi::OsStrExt;
+    let mut res = file_stem.as_os_str().to_owned();
+    let mult_string = mult
+        .iter()
+        .map(|(id, n)| format!("{n}_{}", name(*id)))
+        .join("_");
+    res.push(OsStr::from_bytes(mult_string.as_bytes()));
+    res.push(suffix.as_os_str());
+    Path::new(&res).to_path_buf()
+}
+
+fn multiplicities(event: &Event) -> Vec<(ParticleID, usize)> {
+    event
+        .outgoing()
+        .iter()
+        .map(|(id, p)| (*id, p.len()))
+        .collect()
+}
+
+// TODO: code duplication with `cres.rs`
+fn name(t: ParticleID) -> String {
+    use cres::cluster;
+    t.name()
+        .map(|n| format!("{n}s"))
+        .unwrap_or_else(|| match t {
+            cluster::PID_JET => "jets".to_string(),
+            cluster::PID_DRESSED_LEPTON => "dressed leptons".to_string(),
+            cluster::PID_ISOLATED_PHOTON => "isolated photons".to_string(),
+            _ => format!("particles with id {}", t.id()),
+        })
+}
+
+#[cfg(unix)]
+fn split_filename(p: &Path) -> [&Path; 3] {
+    use std::ffi::OsStr;
+    use std::os::unix::ffi::OsStrExt;
+
+    let p = OsStrExt::as_bytes(p.as_os_str());
+    let start_filename = p
+        .iter()
+        .rposition(|&c| c == b'/')
+        .map(|p| p + 1)
+        .unwrap_or(0);
+    let (directory, filename) = p.split_at(start_filename);
+    // find first dot that is *not* the first character
+    let filename_end = filename
+        .iter()
+        .skip(1)
+        .position(|&c| c == b'.')
+        .map(|p| p + 1);
+    let (stem, suffix) = if let Some(filename_end) = filename_end {
+        filename.split_at(filename_end)
+    } else {
+        (filename, &[] as _)
+    };
+    [directory, stem, suffix].map(|p| Path::new(OsStr::from_bytes(p)))
+}
